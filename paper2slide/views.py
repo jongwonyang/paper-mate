b09f7816f8852bd25101e5b67da6cead8a012407
KIMIS12 <inseo09876@gmail.com>
2023-03-23 22:50:13 +0900

    get_slide_basic

--- paper2slide/views.py
+++ paper2slide/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+import json
+from pptx import Presentation
 
 # TODO: Jongwon
 def index(request):
@@ -59,4 +61,30 @@ def generate_slide(paper_summary):
     ])
     :return: pptx slide file
     """
-    pass
+    paper_summary = json.loads(paper_summary)   #문자열 paper_summary를 json객체로 변경
+    prs = Presentation()                        #새 슬라이드 생성
+
+    """
+    일단 slide_info의 가장 기본형으로 가정하고 개발
+    slide_info[0]: Title 페이지
+    slide_info[1]: Title and content
+    slide_info[2~n-1]: 부제, 내용
+    slide_info[n]: Q & A page
+    """
+
+    for slide_info in paper_summary:                 #slide_info에는 각 slide페이지에 대한 정보가 담겨져 있음
+        slide = prs.slides.add_slide(slide_info['layout'])
+        """
+        slide layout
+        0: Title
+        1: Title and Content
+        2: Section Header (sometimes called Segue)
+        3: Two Content (side by side bullet textboxes)
+        4: Comparison (same but additional title for each side by side content box)
+        5: Title Only
+        6: Blank
+        7: Content with Caption
+        8: Picture with Caption
+        """
+        if slide_info['layout'] == 0:
+            pass
