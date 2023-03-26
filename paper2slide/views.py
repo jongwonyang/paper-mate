c3ead00c7e0e446aa018760a0ef6a50d438ed25f
KIMIS12 <inseo09876@gmail.com>
2023-03-26 15:05:01 +0900

    generate_basic_layout

--- paper2slide/views.py
+++ paper2slide/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+import collections
+import collections.abc
 import json
 from pptx import Presentation
 
 # TODO: Jongwon
+
+
 def index(request):
     return HttpResponse("P2S index.")
 
 # TODO: Heejae
+
+
 def pdf_to_text(pdf_file):
     """
     Divide given paper (pdf file) into sections,
@@ -40,57 +46,62 @@ def pdf_to_text(pdf_file):
     pass
 
 # TODO: Inseo
+
+
+slide_layout = {"Title": 0,
+                "Title and Content": 1,
+                "Section Header": 2,
+                "Two Content": 3,
+                "Comparison": 4,
+                "Title Only": 5,
+                "Blank": 6,
+                "Contnet with Caption": 7,
+                "Picture with Caption": 8}
+
+
 def generate_slide(paper_summary):
-    """
-    Generate slide with given paper summary information.
+    paper_summary = json.loads(paper_summary)
+    prs = Presentation()  # 새 슬라이드 생성
 
-    :param paper_summary: summarized paper contents
-    (The following format is just an example. You can define better format.)
-    (e.g. [
-        {
-            'title': 'Introduction',
-            'summary': ['This paper is ...', 'We discovered that ...', ...],
-            'figure': ['/path/to/file', '/path/to/file', ...]
-        },
-        {
-            'title': 'Overview of CFS and ULE',
-            'subtitle': 'Linux CFS',
-            'summary': ['Per-core scheduling: Linuxs CFS implements ...', ...],
-            'figure': ['/path/to/file', ...]
-        }
-    ])
-    :return: pptx slide file
-    """
-    paper_summary = json.loads(paper_summary)   #문자열 paper_summary를 json객체로 변경
-    prs = Presentation()                        #새 슬라이드 생성
+    name = paper_summary["UserInfo"]["UserID"] + \
+        "_" + paper_summary["UserInfo"]["RequestID"]
 
-    """
-    일단 slide_info의 가장 기본형으로 가정하고 개발
-    slide_info[0]: Title 페이지
-    slide_info[1]: Title and content
-    slide_info[2~n-1]: 부제, 내용
-    slide_info[n]: Q & A page
-    """
-    name = paper_summary['user_name']
-    for slide_info in paper_summary:                 #slide_info에는 각 slide페이지에 대한 정보가 담겨져 있음
-        slide = prs.slides.add_slide(slide_info['layout'])
-        """
-        slide layout
-        0: Title
-        1: Title and Content
-        2: Section Header (sometimes called Segue)
-        3: Two Content (side by side bullet textboxes)
-        4: Comparison (same but additional title for each side by side content box)
-        5: Title Only
-        6: Blank
-        7: Content with Caption
-        8: Picture with Caption
-        """
-        if slide_info['layout'] == 0:
-            title = slide.shapes.title
-            subtitle = slide.placehoders[1]
+    for slide_info in paper_summary["slideInfos"]:
+        layout = prs.slide_layouts[slide_layout[slide_info["layout"]]]
+        slide = prs.slides.add_slide(layout)
 
-            title.text = slide_info['contents']['title']
-            subtitle.text = slide_info['contents']['title']
+        for placeholder_info in slide_info["placeholder"]:
+
+            # print("\n placeholder_info: ", placeholder_info)
+            # print("placeholder_info type: ", type(placeholder_info), "\n")
+
+            placeholder_type = placeholder_info["type"]
+
+            if (placeholder_type == "title"):
+                slide.placeholders[placeholder_info["number"]
+                                   ].text = placeholder_info["content"][0]["item"]
+
+            elif (placeholder_type == "subtitle"):
+                slide.placeholders[placeholder_info["number"]
+                                   ].text = placeholder_info["content"][0]["item"]
+
+            elif (placeholder_type == 'contents placeholder'):
+                body = slide.shapes.placeholders[placeholder_info["number"]]
+                tf = body.text_frame
+                for content in placeholder_info["content"]:
+                    # print("\n content: ", content, "\n")
+                    if (content["type"] == "text"):
+                        p = tf.add_paragraph()
+                        p.text = content["item"]
+                        p.level = content["level"]
+
+            elif (placeholder_type == "text placeholder"):
+                slide.placeholders[placeholder_info["number"]
+                                   ].text = placeholder_info["content"][0]["item"]
+
+            elif (placeholder_type == "picture placeholder"):
+                # print("picture name: ", placeholder_info["content"][0]["item"])
+                slide.placeholders[placeholder_info["number"]].insert_picture(
+                    placeholder_info["content"][0]["item"])
 
     prs.save(name+'.pptx')
