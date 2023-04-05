9af53c89dc0e1f9041fcca7e72ad99184e7d1318
jongwonyang <yjw963@gmail.com>
2023-04-05 19:37:20 +0900

    main page revision. move jstoolchain.

--- paper_mate/settings.py
+++ paper_mate/settings.py
@@ -119,6 +119,10 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
